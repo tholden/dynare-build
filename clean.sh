1a325efebb41e7769db66e47e3a30cbaf3b3e8e3
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2017-02-21 07:44:05 +0100

    Fixed headers (license).

--- clean.sh
+++ clean.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# (C) DynareTeam 2017
+#
+# This file is part of dynare-build project. Sources are available at:
+#
+#     https://gitlab.com/DynareTeam/dynare-build.git
+#
+# Dynare-build is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dynare-build is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
 rm -f win/*.exe
 rm -f zip/*.zip
 rm -f tar/*.tar.xz
